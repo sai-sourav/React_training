a66df277b60b35499ed49c6d26f428e6aba466ad
sai-sourav <saisourav123@gmail.com>
2023-02-04 16:08:28 +0530

    added filter logic code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,15 +5,17 @@ import Card from '../UI/Card';
 import { useState } from "react";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = selectedyear => {
     setFilteredYear(selectedyear);
   }
+
+  const filteredExpenses = props.expenses.filter((expense) => {return expense.ExpenseDate.getFullYear().toString() === filteredYear })
   return (
     <Card className="expenses">
       <Expensesfilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.expenses.map((expense,index) => {
+      {filteredExpenses.map((expense,index) => {
         return (
           <ExpenseItem key={index+1}
             ExpenseDate={expense.ExpenseDate}
